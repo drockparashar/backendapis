6e11c0068c7fcaf3799d534c3a05ed31ee935543
drockparashar <pranshu007parashar@gmail.com>
2025-05-05 02:41:23 +0530

    Added backend hosted link

--- Nursery/client/src/lib/api.js
+++ Nursery/client/src/lib/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://backend-apis-00fo.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
